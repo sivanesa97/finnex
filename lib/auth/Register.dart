9a6aff9b1fc960f693acf31cda700a3be167c76a
sivanesa97 <sivanesa007@gmail.com>
2022-03-27 00:07:45 +0530

    Commit by Roger

---
+++ lib/auth/Register.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/auth/SignIn.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
