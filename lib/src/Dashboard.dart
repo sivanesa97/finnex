67d9714fc106377d508d29d20c7c9cf54ea1d0b6
sivanesa97 <sivanesa007@gmail.com>
2022-03-27 09:53:53 +0530

    Commit by Roger

--- lib/src/Dashboard.dart
+++ lib/src/Dashboard.dart
@@ -1,14 +1,23 @@
+import 'package:finnex/auth/smsPermission.dart';
 import 'package:flutter/material.dart';
+import 'package:sms/sms.dart';
 
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
 }
 
+@override
+void initState() async {
+  List messages = [];
+  SmsQuery query = new SmsQuery();
+  await query.getAllSms.then((value) => {print(value.length)});
+}
+
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold();
   }
 }
